4d355b4da10814e5d93609e70306c681804c79a9
Pabloarro <pablo.arroyuelos@opendeusto.es>
2023-06-22 19:43:21 +0200

    commit

--- IKEADE_SERVER/DB.h
+++ IKEADE_SERVER/DB.h
@@ -5,16 +5,3 @@
  *      Author: pablo
  */
 
-#ifndef BBDD_H_
-#define BBDD_H_
-
-#include "sqlite3.h"
-
-int crearTablas();
-int insertarData(char* nom, char* pass);
-int insertPart(char* tipo, char* nom);
-int llamada(void *, int, char **, char **);
-int mostrarData();
-int checkPass(char* nom, char* pass);
-
-#endif
